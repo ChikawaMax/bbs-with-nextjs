ec886e312803177592f3f9c24e67a38d4364ac05
本間敬太 <keitakunv2@gmail.com>
2024-06-15 18:55:42 +0900

    🐛

--- app/actions/postBBSaction.ts
+++ app/actions/postBBSaction.ts
@@ -1,16 +1,25 @@
-"use server"
+"use server";
 
-import { z } from "zod"
-import { formSchema } from "../bbs-posts/create/page"
-import prisma from "@/lib/prismaClient"
-import { revalidatePath } from "next/cache"
-import { redirect } from "next/navigation"
+import { z } from "zod";
+import { formSchema } from "../bbs-posts/create/page";
+import prisma from "../../lib/prismaClient";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
-export const postBBS = async ({ username, title, content }: z.infer<typeof formSchema>) => {
+export const postBBS = async ({
+    username,
+    title,
+    content,
+}: z.infer<typeof formSchema>) => {
     await prisma.post.create({
-        data: { username, title, content }
+        data: {
+            username,
+            title,
+            content,
+        },
     });
-}
 
-revalidatePath("/");
-redirect("/")
+    revalidatePath("/");
+
+    redirect("/");
+};
